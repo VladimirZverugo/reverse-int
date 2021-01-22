a4bedf54b264dfb7af36d8cbc7083b370ca5b2d7
Vladimir <44143087+VladimirZverugo@users.noreply.github.com>
2021-01-22 17:08:13 +0300

    init: upload task "reverse-int"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let nNumberString = "";
+    n = Math.abs(n);
+    nNumberString = n.toString().split("").reverse().join("");
+
+    return nNumberString;
 }
